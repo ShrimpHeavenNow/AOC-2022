2639ff38a05526c1a5ffda164b47371a02790e5e
Dean <wrightde@gmail.com>
2022-11-30 21:46:12 -0800

    UUUH

--- Day1.py
+++ Day1.py
@@ -1,3 +1,22 @@
-print("Hello Farts!")
-#  test?
-print("glonkle")
+with open('calories.txt') as f:
+    sweeps = [line.rstrip().split("\n\n") for line in f]  #  This variable name is from day one of 2021
+
+entry =[]
+entries = []
+for x in sweeps:  #  I KNOW theres a way to do this in the with loop and I think i've done it before.
+    if x[0] != "":
+        entry.append(int(x[0]))
+    else:
+        entries.append(entry)
+        entry = []
+
+totals = []
+for x in entries:
+    current = 0
+    for y in x:
+        current += y
+    totals.append(current)
+print("Part One: ", max(totals))  
+totals.sort()
+totals.reverse()
+print("Part Two: ", totals[0]+totals[1]+totals[2])
